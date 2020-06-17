19271585895d24bec9775562f78b89498d0d32c5
JolinTeo <64124299+JolinTeo@users.noreply.github.com>
2020-06-17 20:44:49 +0800

    Updated by Jolin
    Enhanced GamePlay

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
 
+    public Rigidbody rb;
+
+    public float velocity = 0;
+
+    public int Score = 0;
+
+    public Text Score_Txt;
+
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = Vector2.up * velocity;
             thisAnimation.Play();
+        }
+
+        if(Score >= 150)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+            
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "ScoreZone")
+        {
+            Score += 10;
+            Score_Txt.text = "Score: " + Score;
+        }
     }
 }
